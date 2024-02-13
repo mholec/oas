4842877c13eb4144ffc37d4b1233cf6668ee7721
miroslavholec <mirek@miroslavholec.cz>
2024-02-13 09:32:37 +0100

    Senzory.yaml

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzNDY4MA/spectral.js?branch_slug=main&token=4227acfe-874b-4aec-a76f-f1f33f4cb42f";
+export default { extends: ruleset };
